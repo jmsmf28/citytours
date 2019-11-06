90abafc28b596c3c411d331686fd4f2f75a1720a
jmsmf28 <jmsmf2011@gmail.com>
2019-11-06 12:01:23 +0000

    added Tour and Tourlist

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import './Tour.scss';
-import { FontAwesomeIcon } from '@fortawesome/fontawesome-free';
+//import { FontAwesomeIcon } from '@fortawesome/fontawesome-free';
+import imgDefault from '../../img/london.jpeg';
 
 export default class Tour extends Component {
     render() {
         return (
-            <article>
+            <article className="tour">
                 <div className="img-container">
-                    <img width="200" src='https://images.pexels.com/photos/2965505/pexels-photo-2965505.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt="" />
+                    <img src={imgDefault} alt="" />
+                    <span className="close-btn">x
+                        <i className="fas fa-window-close" />
+                    </span>
                 </div>
-                <span className="close-btn">
-                    <i className="fas fa-window-close" />
-                    <FontAwesomeIcon icon="" />
-                </span>
+                <div className="tour-info">
+                    <h3>city</h3>
+                    <h4>name</h4>
+                    <h5>info <span className="">v<i className="fas fa-caret-square-down" /></span></h5>
+                    <p>lorem ipsum dolor sit maet consecteur 
+                        adpsicing elite. Harum debitis.</p>
+                </div>              
             </article>
           
         )
--- src/components/TourList/TourList.js
+++ src/components/TourList/TourList.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './TourList.scss';
 import Tour from '../Tour/Tour';
+import { tourData } from '../../tourData';
+import { createUnionTypeAnnotation } from '@babel/types';
 
 export default class TourList extends Component {
+    state={
+        tours: tourData
+    };
+    /*{ city, name, img, info} = {tours};*/
     render() {
+        const {tours} = this.state;
         return (
-            <section>
-                <Tour />
+            <section className="tourlist">
+             
             </section>
         )
     }
