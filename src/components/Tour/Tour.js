fc40e1199bf26fd0f74305661a80b8e2d6a2a548
jmsmf28 <jmsmf2011@gmail.com>
2019-11-06 16:47:03 +0000

    final with local data

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import './Tour.scss';
-//import { FontAwesomeIcon } from '@fortawesome/fontawesome-free';
-import imgDefault from '../../img/london.jpeg';
 
 export default class Tour extends Component {
+    state={
+        showInfo: false
+    }
+    handleInfo = () =>{
+        this.setState({
+            showInfo:!this.state.showInfo
+        });
+    }
     render() {
+        const { id, city, img, name, info} = this.props.tour;
+        const { removeTour } = this.props;
         return (
             <article className="tour">
                 <div className="img-container">
-                    <img src={imgDefault} alt="" />
-                    <span className="close-btn">x
+                    <img src={img} alt="" />
+                    <span className="close-btn" onClick={()=>{removeTour(id)}}>
                         <i className="fas fa-window-close" />
                     </span>
                 </div>
                 <div className="tour-info">
-                    <h3>city</h3>
-                    <h4>name</h4>
-                    <h5>info <span className="">v<i className="fas fa-caret-square-down" /></span></h5>
-                    <p>lorem ipsum dolor sit maet consecteur 
-                        adpsicing elite. Harum debitis.</p>
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
+                    <h5><span onClick={this.handleInfo}>
+                        <i className="fas fa-caret-square-down" />
+                        </span>
+                    </h5>
+                    {this.state.showInfo && <p>{info}</p>}
                 </div>              
             </article>
           
--- src/components/TourList/TourList.js
+++ src/components/TourList/TourList.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import './TourList.scss';
 import Tour from '../Tour/Tour';
 import { tourData } from '../../tourData';
-import { createUnionTypeAnnotation } from '@babel/types';
 
 export default class TourList extends Component {
     state={
         tours: tourData
     };
-    /*{ city, name, img, info} = {tours};*/
+    removeTour = id => {
+        const{tours} = this.state;
+        const sortedTours = tours.filter(tour=> tour.id !=id);
+        this.setState({
+            tours:sortedTours
+        })
+    }
     render() {
         const {tours} = this.state;
         return (
             <section className="tourlist">
-             
+                {tours.map(tour =>{   // tour can have any name that we want
+                    return <Tour key={tour.id} tour= {tour} removeTour={this.removeTour} />
+                })}
             </section>
         )
     }
